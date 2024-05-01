e267e623728aaf6c830514c0dc664ecd2babdfc8
Faguim <fagnermuniz12@gmail.com>
2024-05-01 16:13:38 -0300

    project initial created

---
+++ README.md
@@ -0,0 +1,14 @@
+# Filmm App
+
+<p>Aplicativo android com o intuito de pesquisar por filmes</p>
+
+## Funcionalidades:
+- Buscar por filmes
+- Ver detalhes do filme
+- Salvar no dispositivo os filmes favoritos
+
+## O foi utilizado:
+- Kotlin com JetPack Compose
+- Courontimes
+- Retrofit
+- Room
---
+++ app/src/main/java/com/guim/filmmapp/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.guim.filmmapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.guim.filmmapp.ui.theme.FilmmAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            FilmmAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+
+                }
+            }
+        }
+    }
+}
