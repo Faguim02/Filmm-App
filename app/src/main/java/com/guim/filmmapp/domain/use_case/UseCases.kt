f30b47eddda6bed4c488f27fea78de5855bf2329
Faguim <fagnermuniz12@gmail.com>
2024-05-04 18:33:52 -0300

    refactor: add cases from MovieApi for useCases

--- app/src/main/java/com/guim/filmmapp/domain/use_case/UseCases.kt
+++ app/src/main/java/com/guim/filmmapp/domain/use_case/UseCases.kt
@@ -2,5 +2,9 @@ package com.guim.filmmapp.domain.use_case
 
 data class UseCases(
     val getMovieDataUseCase: GetMovieDataUseCase,
-    val getSearchDataUseCase: GetSearchDataUseCase
+    val getSearchDataUseCase: GetSearchDataUseCase,
+    val addMovieDataUseCase: AddMovieDataUseCase,
+    val removeMovieDataUseCase: RemoveMovieDataUseCase,
+    val findOneMovieDataCaseUse: FindOneMovieDataCaseUse,
+    val findAllMovieDataCaseUse: FindAllMovieDataCaseUse
 )
