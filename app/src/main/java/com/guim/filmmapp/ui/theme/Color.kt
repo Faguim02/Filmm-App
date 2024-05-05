3ebd60ec9cccbbd8fa5d40454982db0036d214c9
Faguim <fagnermuniz12@gmail.com>
2024-05-04 22:11:41 -0300

    style: adapted theme from app

--- app/src/main/java/com/guim/filmmapp/ui/theme/Color.kt
+++ app/src/main/java/com/guim/filmmapp/ui/theme/Color.kt
@@ -2,10 +2,10 @@ package com.guim.filmmapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val White = Color.White
+val Black = Color.Black
+val BlueLight = Color(0xFFF0EFF4)
+val Purple = Color(0xFF832161)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val PurpleDark = Color(0xFF1A161C)
+val PurpleGrey = Color(0xFF221D24)
